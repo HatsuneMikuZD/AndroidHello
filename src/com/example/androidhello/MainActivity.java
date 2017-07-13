5a5dacea2c5c3cff6ae3ffae18406ab5694390cc
HatsuneMikuZD <1982714031@qq.com>
2017-07-13 10:34:08 +0800

    修改成功

--- src/com/example/androidhello/MainActivity.java
+++ src/com/example/androidhello/MainActivity.java
@@ -2,8 +2,6 @@ package com.example.androidhello;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
@@ -11,6 +9,6 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		int i = 10;
+		
 	}
 }
